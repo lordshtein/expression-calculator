4bc7d52532885e351de09b9ceb823c409580c2b8
lordshtein <lordshtein@gmail.com>
2020-03-12 20:11:41 +0300

    feat: task done (can be refactored)

--- src/index.js
+++ src/index.js
@@ -5,24 +5,49 @@ function eval() {
 
 function expressionCalculator(expr) {
   /* убрать пробелы */
-  expr = expr.replace(/\s+/g, '');
-  let arrayStep1 = expr.match(/[\(\)\*\/\+\-0-9]/g)
+  
+  
   let result = []
   let operators = []
   let solved = []
   let array = []
   let str =""
   let checkBrackets = []
+
+  /* проверка на непарные скобки */
+  checkBrackets = expr.match(/[\(\)]/g)
+
+  if (checkBrackets) {
+    let check = []
+    checkBrackets.forEach( function (e) {
+      if ( e == ")" && check[check.length-1] == "(") {
+        check.pop()
+      } else {
+        check.push(e)
+      }
+    })
+    if (check.length) {
+    throw "ExpressionError: Brackets must be paired"
+  }
+  }
+
   
   /* соеденить соседние цифры */
+  expr = expr.replace(/\s+/g, '');
+  let arrayStep1 = expr.match(/[\(\)\*\/\+\-0-9]/g)
+
+  
 
   for (let i=0; i<arrayStep1.length; i++) {
     if (arrayStep1[i] == "+" || arrayStep1[i] == "-" 
     || arrayStep1[i] == "*" || arrayStep1[i] == "/"
-    || arrayStep1[i+1] == "(" || arrayStep1[i+1] == ")") {
+    || arrayStep1[i] == "(" || arrayStep1[i] == ")") {
+      
       array.push(arrayStep1[i])
+      
 
     } else {
+      
       str=arrayStep1[i]
 
       while (parseFloat(arrayStep1[i+1]) || arrayStep1[i+1] == "0") {
@@ -32,20 +57,11 @@ function expressionCalculator(expr) {
       array.push(str)
     }  
   }
-  /* проверка деления на 0 */
-
-
-
-  /* проверка на непарные скобки */
-  
-
-
-  console.log(array)
 
 
   for (let i = 0; i < array.length; i++) {
 
-    if (Number(array[i])) {
+    if (Number(array[i]) || array[i] == "0") {
       result.push(array[i])
 
     } else if (array[i] == "(") {
@@ -76,7 +92,7 @@ function expressionCalculator(expr) {
 
 
   for (let i = 0; i < result.length; i++) {
-    if (parseFloat(result[i])) {
+    if (parseFloat(result[i]) || result[i] == "0") {
       solved.push(parseFloat(result[i]))
     } else {
       let a = solved.pop();
@@ -88,13 +104,17 @@ function expressionCalculator(expr) {
       } else if (result[i] === "*") {
         solved.push(parseFloat(a) * parseFloat(b));
       } else if (result[i] === "/") {
-        if (a == "0") return "TypeError: Division by zero."
+        /* проверка деления на 0 */
+        if (a == "0") {
+          throw "TypeError: Division by zero." 
+        }
         solved.push(parseFloat(b) / parseFloat(a));
       }
     }
   }
-  console.log(parseFloat(solved[0]))
   return parseFloat(solved[0]) 
+
+  /* sorry for so long way */
 }
 
 
